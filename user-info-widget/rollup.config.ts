002a64328997bc85b745280b0121d77547015b2f
chirag-purohit <chirag-purohit>
2025-05-01 19:13:39 +0530

    changes

--- user-info-widget/rollup.config.ts
+++ user-info-widget/rollup.config.ts
@@ -1,14 +1,22 @@
+// rollup.config.ts
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
+import babel from '@rollup/plugin-babel';
 
 export default {
   input: 'src/index.ts',
-  external: ['react', 'zustand'],
+  // external will treat any module matching the regex or the string as external
+  external: [/^react($|\/)/, 'zustand'],
   plugins: [
     resolve(),
     commonjs(),
-    typescript({ useTsconfigDeclarationDir: true })
+    typescript({ useTsconfigDeclarationDir: true }),
+    babel({
+      babelHelpers: 'bundled',
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      presets: [['@babel/preset-react', { runtime: 'classic' }]]
+    })
   ],
   output: [
     { file: 'dist/index.cjs.js', format: 'cjs', sourcemap: true },
